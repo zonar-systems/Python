4c9fd840b495e3fa564f49e1df6143dda3c84bf2
zonar-systems <rob.mossuto@zonarsystems.com>
2015-11-19 11:12:02 -0800

    Update addassets.py
    Update request data

--- addassets.py
+++ addassets.py
@@ -3,7 +3,7 @@ import urllib.request
 
 # build request string
 response = urllib.request.urlopen(
-    'https://qa1.zonarsystems.net/interface.php?username=rmossuto&password=Password.123&action=adminassets&operation=add&format=xml&name=DEMO12345&vin=12345678901&fleet=DEMO123456&exsid=9498&location=Seattle&type=Standardlogvers=2')
+    'https://qa1.zonarsystems.net/interface.php?username=&password=&action=adminassets&operation=add&format=xml&name=DEMO12345&vin=12345678901&fleet=DEMO123456&exsid=9498&location=Seattle&type=Standardlogvers=2')
 # read response
 data = response.read()
 assert isinstance(data, object)
