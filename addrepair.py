bfa24b1183d6dbf5255bafff3cbaa67453e7b20c
rmossuto <rmossuto@outlook.com>
2015-11-23 11:05:28 -0800

    New methods added

--- addrepair.py
+++ addrepair.py
@@ -3,7 +3,7 @@ import urllib.request
 
 # build request string
 response = urllib.request.urlopen(
-    'https://qa1.zonarsystems.net/interface.php?username=&password=&action=addrepair&mechanic=jdoe&repair_date=1042841042&inspid=27&sonumber=SON2121&comment=Replaced&defectid=2021&format=xml')
+    'https://qa1.zonarsystems.net/interface.php?username=&password=Password=&action=addrepair&mechanic=jdoe&repair_date=1042841042&inspid=27&sonumber=SON2121&comment=Replaced&defectid=2021&format=xml')
 # read response
 data = response.read()
 assert isinstance(data, object)
